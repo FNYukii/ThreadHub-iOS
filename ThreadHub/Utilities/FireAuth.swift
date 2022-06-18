b2635a93a17f9aabcd11061eafe4a6de61cd173e
Yu357 <forestgreen14689@gmail.com>
2022-06-18 14:38:10 +0900

    feat: 匿名認証に成功

--- ThreadHub/Utilities/FireAuth.swift
+++ ThreadHub/Utilities/FireAuth.swift
@@ -9,6 +9,18 @@ import Firebase
 
 class FireAuth {
     
+    static func signIn() {
+        Auth.auth().signInAnonymously { authResult, error in
+            if let error = error {
+                print("HELLO! Fail! Error: \(error)")
+            }
+            
+            if let authResult = authResult {
+                print("HELLO! Success! Signed in. uid: \(authResult.user.uid)")
+            }
+        }
+    }
+    
     static func userId() {
         // TODO: Return uid
     }
