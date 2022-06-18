d40f4dfa892a20a833905734d949119eb3de4cc9
Yu357 <forestgreen14689@gmail.com>
2022-06-18 13:18:15 +0900

    Initial Commit

---
+++ ThreadHub/ThreadHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThreadHubApp.swift
+//  ThreadHub
+//
+//  Created by Yu on 2022/06/18.
+//
+
+import SwiftUI
+
+@main
+struct ThreadHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
