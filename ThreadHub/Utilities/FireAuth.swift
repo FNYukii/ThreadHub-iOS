781170842c3575c3d1bdced95a850f55ad621e4a
Yu357 <forestgreen14689@gmail.com>
2022-06-18 14:34:20 +0900

    feat: データベース操作用のクラスを作成

---
+++ ThreadHub/Utilities/FireAuth.swift
@@ -0,0 +1,15 @@
+//
+//  FireAuth.swift
+//  ThreadHub
+//
+//  Created by Yu on 2022/06/18.
+//
+
+import Firebase
+
+class FireAuth {
+    
+    static func userId() {
+        // TODO: Return uid
+    }
+}
---
+++ ThreadHub/Utilities/FireThread.swift
@@ -0,0 +1,15 @@
+//
+//  FireThread.swift
+//  ThreadHub
+//
+//  Created by Yu on 2022/06/18.
+//
+
+import Firebase
+
+class FireThread {
+    
+    static func createThread(displayName: String, title: String, detail: String) {
+        // TODO: Add a document
+    }
+}
