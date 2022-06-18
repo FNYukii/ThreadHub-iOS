7da3d720d171e0b42d43edb82ba8c96366547237
Yu357 <forestgreen14689@gmail.com>
2022-06-18 15:22:32 +0900

    feat: ThreadViewへ遷移するようにした

---
+++ ThreadHub/Components/CommentRow.swift
@@ -0,0 +1,14 @@
+//
+//  CommentRow.swift
+//  ThreadHub
+//
+//  Created by Yu on 2022/06/18.
+//
+
+import SwiftUI
+
+struct CommentRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
