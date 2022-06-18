5e3e9db9325ce2c537dcb657e69364c3b5f34934
Yu357 <forestgreen14689@gmail.com>
2022-06-18 13:49:58 +0900

    feat: Thread構造体を作成

---
+++ ThreadHub/Models/Thread.swift
@@ -0,0 +1,26 @@
+//
+//  Thread.swift
+//  ThreadHub
+//
+//  Created by Yu on 2022/06/18.
+//
+
+import Firebase
+
+struct Thread {
+    let id: String
+    let createdAt: Date
+    let userId: String
+    let displayName: String
+    let title: String
+    let detail: String
+    
+    init(document: QueryDocumentSnapshot) {
+        self.id = document.documentID
+        self.createdAt = (document.get("createdAt") as! Timestamp).dateValue()
+        self.userId = document.get("userId") as! String
+        self.displayName = document.get("displayName") as! String
+        self.title = document.get("title") as! String
+        self.detail = document.get("detail") as! String
+    }
+}
