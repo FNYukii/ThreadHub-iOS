a0ca839946ba38aff817f9bae474c51df392ca02
Yu357 <forestgreen14689@gmail.com>
2022-06-18 15:24:37 +0900

    feat: Comment構造体を作成

---
+++ ThreadHub/Entities/Comment.swift
@@ -0,0 +1,26 @@
+//
+//  Comment.swift
+//  ThreadHub
+//
+//  Created by Yu on 2022/06/18.
+//
+
+import Firebase
+
+struct Comment: Identifiable {
+    let id: String
+    let threadId: String
+    let createdAt: Date
+    let userId: String
+    let displayName: String
+    let text: String
+    
+    init(document: QueryDocumentSnapshot) {
+        self.id = document.documentID
+        self.threadId = document.get("threadId") as! String
+        self.createdAt = (document.get("createdAt", serverTimestampBehavior: .estimate) as! Timestamp).dateValue()
+        self.userId = document.get("userId") as! String
+        self.displayName = document.get("displayName") as! String
+        self.text = document.get("text") as! String
+    }
+}
